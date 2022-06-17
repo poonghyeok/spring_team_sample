8ad26b53d48446fb05c1908138f4630e35cc8d00
kijin <sungkijin@gmail.com>
2022-06-17 13:01:59 +0900

    kijin bean6

--- src/sample03/HelloSpring.java
+++ src/sample03/HelloSpring.java
@@ -34,6 +34,10 @@ public class HelloSpring {
 		MessageBean messageBean5 = (MessageBean)context.getBean("messageBean");
 		messageBean.sayHello("Goodbye");
 		System.out.println("==================================");
+
+		MessageBean messageBean6 = (MessageBean)context.getBean("messageBean");
+		messageBean.sayHello("Kijin Bean 6 ");
+		System.out.println("==================================");
 		
 		//각각의 객체를 생성했음에도 불구하고 같은 field를 증가시켰다. 왜  why
 		//
